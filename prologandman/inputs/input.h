50c1b41bf58df827141c18419c4dd60a6a718977
vishal124788 <vishal124788@gmail.com>
2021-02-16 01:26:49 +0530

    Status changed

---
+++ prologandman/inputs/input.h
@@ -0,0 +1,15 @@
+
+#include<iostream>
+#include<string>
+#include<fstream>
+
+class debugInput{
+    private:
+
+};
+
+class perserInput{
+    private:
+
+
+};
---
+++ prologandman/main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<string>
+
+
+int main(void){
+    std::cout<<"This is a program for Testing VTS 1.0" << std::endl;
+
+}  
